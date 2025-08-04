06c62e92ed0fed4a0204b570f56b3b5ca21ef09c
GXT <tudorghetiu1@gmail.com>
2025-08-04 17:19:31 +0300

    Create approve.js

---
+++ NaughtCoin/Bypass/approve.js
@@ -0,0 +1,4 @@
+await contract.approve('0x230E0830e4e285679fc010F1f12616017b76bd60', await contract.balanceOf(player))
+
+//this call approves the contract at the above specified address to spend the amount balanceOf(player)
+//basically giving the contract full power over my entire holdings 
